d96034e6fbf361f23436d065ff501bbf7a01ea2e
Shuvam Banerjee <156934625+iamshuvi@users.noreply.github.com>
2026-07-09 03:59:49 +0530

    sidebar toggle

--- platform/sddi/assets/js/app.js
+++ platform/sddi/assets/js/app.js
@@ -1,1 +1,14 @@
-console.log("AMARTRACK Government Design System Loaded");
+const toggle = document.getElementById('toggleSidebar');
+const sidebar = document.querySelector('.sidebar');
+const main = document.querySelector('.main');
+
+if (toggle && sidebar && main) {
+
+    toggle.addEventListener('click', () => {
+
+        sidebar.classList.toggle('collapsed');
+        main.classList.toggle('expanded');
+
+    });
+
+}
